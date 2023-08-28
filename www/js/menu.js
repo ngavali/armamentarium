7cf0f0530e9ed9905ee35d62793966dc895cd39c
ngavali <ngavali@akamai.com>
2023-08-28 10:04:27 +0530

    Collapsable

--- www/js/menu.js
+++ www/js/menu.js
@@ -206,4 +206,22 @@ _().ready(function() {
         }
     
         fixFont(getReadingMode())
+    //Collapsable content
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
 });
+
+
